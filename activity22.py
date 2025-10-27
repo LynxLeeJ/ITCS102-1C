398d9395199e70c8494a62818441bed1e86c3ea6
Jet Lee Lim <limjetlee1@gmail.com>
2025-10-27 13:47:25 +0800

    Add files via upload

---
+++ activity22.py
@@ -0,0 +1,16 @@
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+print(months)
+#for i in months:
+#   print(f"Month - {i}, 2025")
+
+months.reverse()
+print(months)
+
+fullname = 'Jet Lee'
+
+new_list = list(fullname)
+new_list.reverse()
+print(new_list)
+
+new_list.sort() 
+print(new_list)
